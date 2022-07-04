22b9c0f5ecca577ca7df109b1ba24db0abb495ca
NicholasTavares <nicholasbalby@hotmail.com>
2022-07-04 20:05:30 -0300

    refactor: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import theme from "./global/theme";
+import GlobalStyle from "./global/styles";
 
 const App: React.FunctionComponent = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div className="App">
         <Home />
       </div>
