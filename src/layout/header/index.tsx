96d0bc5473fc54ae639fc71720dffc594e3f3783
NicholasTavares <nicholasbalby@hotmail.com>
2022-07-05 09:22:51 -0300

    feat: header layout

---
+++ src/layout/header/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  HeaderContainer,
+  HeaderContent,
+  Logo,
+  Option,
+  OptionsContainer,
+  TextOption,
+  UndelineDecorationRed,
+  UndelineDecorationBlue,
+  UndelineDecorationOrange,
+  TextContainer,
+  TextContent,
+  AnimationContainer,
+} from "./styles";
+import logo from "../../assets/logo.png";
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <Logo src={logo} />
+        <OptionsContainer>
+          <Option>
+            <TextOption>Events</TextOption>
+            <UndelineDecorationBlue />
+          </Option>
+          <Option>
+            <TextOption>Characters</TextOption>
+            <UndelineDecorationRed />
+          </Option>
+          <Option>
+            <TextOption>Creators</TextOption>
+            <UndelineDecorationOrange />
+          </Option>
+        </OptionsContainer>
+      </HeaderContent>
+      <TextContainer>
+        <TextContent>Explore Marvel Universe like a real fan!</TextContent>
+        <AnimationContainer />
+      </TextContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/layout/header/styles.ts
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import media from "styled-media-query";
+
+export const HeaderContainer = styled.header`
+  height: 70vh;
+  padding: 1rem;
+  flex-direction: column;
+  background-color: ${({theme}) => theme.colors.dark};
+  position: relative;
+  -webkit-clip-path: polygon(0 0, 100% 0, 100% 55vh, 0 100%);
+  clip-path: polygon(0 0, 100% 0, 100% 55vh, 0 100%);
+  transition: all 500ms;
+
+  ${media.greaterThan("medium")`
+    height: 80vh;
+  `}
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  justify-content: left;
+  margin-bottom: 1rem;
+`
+
+export const Logo = styled.img`
+  width: 5rem;
+  margin-right: 1rem;
+`
+
+export const OptionsContainer = styled.nav`
+  display: flex;
+  align-items: center;
+`
+
+export const Option = styled.div`
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: left;
+
+  &:not(:last-child){
+    margin-right: 1rem;
+  }
+`
+
+export const TextOption = styled.span`
+  color: ${({theme}) => theme.colors.light};
+  font-size: 1.4rem;
+`
+
+export const UndelineDecorationRed = styled.div`
+  transition: width 500ms;
+  width: 1rem;
+  height: 0.2rem;
+  background-color: ${({theme}) => theme.colors.marvel_red};
+
+  ${Option}:hover & {
+    width: 100%;
+  }
+`
+
+export const UndelineDecorationBlue = styled.div`
+  transition: width 500ms;
+  width: 1rem;
+  height: 0.2rem;
+  background-color: ${({theme}) => theme.colors.marvel_blue};
+
+  ${Option}:hover & {
+    width: 100%;
+  }
+`
+
+export const UndelineDecorationOrange = styled.div`
+  transition: width 500ms;
+  width: 1rem;
+  height: 0.2rem;
+  background-color: ${({theme}) => theme.colors.marvel_orange};
+
+  ${Option}:hover & {
+    width: 100%;
+  }
+`
+
+export const TextContainer = styled.div`
+  display: flex;
+`
+export const TextContent = styled.h1`
+  font-size: 2.5rem;
+  text-shadow: 
+               -1px -1px 0px #202020, 
+               -1px 1px 0px #202020,                    
+                1px -1px 0px #202020,                  
+                1px 0px 0px #202020;
+  color: ${({theme}) => theme.colors.light};
+`
+export const AnimationContainer = styled.div`
+  width: 8rem;
+`
