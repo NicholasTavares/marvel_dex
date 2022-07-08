ea2368f36b364fdb40551cc90b303845227ae156
NicholasTavares <nicholasbalby@hotmail.com>
2022-07-07 23:38:26 -0300

    feat: initial config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Home from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import theme from "./global/theme";
 import GlobalStyle from "./global/styles";
+import SystemRoutes from "./routes/app.routes";
 
 const App: React.FunctionComponent = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SystemRoutes />
     </ThemeProvider>
   );
 };
