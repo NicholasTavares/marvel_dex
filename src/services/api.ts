e145a0673fe788d2d777d00cfeb7d27f33b4afa2
NicholasTavares <nicholasbalby@hotmail.com>
2022-07-08 23:25:42 -0300

    refactor: new api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import md5 from "md5";
 
-const publicKey = "3ce21735f07dcd61bfee2a0241651923";
-const privateKey = "bbdf3be4217ee0758cb02cb0b11b14a927f67c4c";
+const publicKey = "74ccde4d6e0be7994619ba239782ce6b";
+const privateKey = "eb8d6f582b242840fc0fa24ca9f19340457589c6";
 const time = Number(new Date());
 const hash = md5(time + privateKey + publicKey);
 
